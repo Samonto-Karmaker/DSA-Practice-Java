7af34f4a9d156ffebb02a5f927d08aaba4edb38f
Samanta Karmaker <samonto0726@gmail.com>
2025-01-06 21:58:40 +0600

    Implemented Trapping Rain Water I

--- src/ArrayAlgo.java
+++ src/ArrayAlgo.java
@@ -66,4 +66,31 @@ public class ArrayAlgo {
         }
         return maxProfit;
     }
+
+    // Trapping Rain Water I
+    // Time complexity: O(n), Space complexity: O(n)
+    // Intuition: find the max height on the left and right of each element
+    // and the water trapped at each element is min(leftMax, rightMax) - height
+    public static int trappingRainWater(int[] arr) {
+        int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int ans = 0;
+
+        left[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], arr[i]); // find the max height on the left
+        }
+
+        right[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0 ; i--) {
+            right[i] = Math.max(right[i + 1], arr[i]); // find the max height on the right
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(left[i], right[i]) - arr[i];
+        }
+
+        return ans;
+    }
 }
